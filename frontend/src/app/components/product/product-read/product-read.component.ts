bc49b8b3bd68e86915c5d9311d6b607c986f8814
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-12-20 20:16:19 -0300

    Adicionando Coluna Ações

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -10,7 +10,7 @@ import { Product } from '../product.model';
 })
 export class ProductReadComponent implements OnInit {
   products: Product[] = [];
-  displayedColumns = ['id', 'name', 'price']
+  displayedColumns = ['id', 'name', 'price', 'action']
 
   constructor(private produtoService: ProductService) { }
 
