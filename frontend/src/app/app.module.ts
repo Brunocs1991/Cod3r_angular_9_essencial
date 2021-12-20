ad675709d41eb6048c5da1348b55c87c1f48e22f
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-12-20 14:57:16 -0300

    Formulário Criar Produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
@@ -18,7 +22,6 @@ import { ForDirective } from './directives/for.directive';
 import { RedDirective } from './directives/red.directive';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-
 
 @NgModule({
   declarations: [
@@ -42,8 +45,11 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
-    HttpClientModule
-
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
